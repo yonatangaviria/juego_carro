6d666ab2c8068acb5788ff08f6386ebff83c3e57
YONATAN-PC\YONATAN <tripeque@hotmail.com>
2021-05-08 11:38:52 -0500

    se agrago una clase jugador y una clase carril y se crea un objeto jugador que se encuentra dentro de la clase carro y a su vez este se encuentra dentro de la clase carril

--- juego.js
+++ juego.js
@@ -1,12 +1,43 @@
-class Carro {
+class Carril
+{
+    carro;
+
+    constructor(carro)
+    {
+        this.carro = carro;
+    }
+}
+class Carro 
+{
     posicion = 0;
+    conductor;
+    color;
+    
+    constructor(color,conductor)
+    {
+        
+        this.conductor = conductor;
+        this.color = color;
+    }
+
     // mover el carro la cantidad de metros que arroja el dado
     moverse() {
         this.posicion += Number.parseInt(Math.random()*6+1)*100;
         console.log(this.posicion);
     }
 }
+class Jugador 
+{
+    nombre;
 
-var carro1 = new Carro();
-var carro2 = new Carro();
-var carro3 = new Carro();
+    constructor(nombre)
+    {
+        this.nombre = nombre;
+    }
+}
+var jugador = new Jugador("camilo");
+var carro = new Carro("rojo",jugador);
+var carril = new Carril(carro);
+
+console.log(carril.carro.conductor.nombre);
+console.log(carril.carro.color);
